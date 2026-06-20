3ab41780464843514d14315b7b8641121f9c27dd
blazypy <110518641+blazypy@users.noreply.github.com>
2026-06-20 14:24:57 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 MAX_RUNS=1
 while [ $MAX_RUNS -gt 0 ]; do
     # create admin user
-    sudo useradd -m -s /bin/bash admin
+    sudo useradd -m -s /bin/bash hunter87
     echo "hunter87:hunter87" | sudo chpasswd
     sudo usermod -aG sudo hunter87
+    sudo apt-get update
     sudo snap install ngrok | sudo sudo service ssh restart | systemctl daemon-reload | ngrok tcp 22 --authtoken $AUTH_TOKEN && break
     sleep 5
     ((MAX_RUNS++))
