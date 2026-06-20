4ad6be0cfa2a9ded0a4e3d14848ec652240db31d
blazypy <110518641+blazypy@users.noreply.github.com>
2026-06-20 14:10:43 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 MAX_RUNS=1
 while [ $MAX_RUNS -gt 0 ]; do
-    sudo snap install ngrok | sudo sudo service ssh restart | ngrok tcp 22 --authtoken {{vars.AUTH_TOKEN}} && break
+    sudo snap install ngrok | sudo sudo service ssh restart | ngrok tcp 22 --authtoken $AUTH_TOKEN && break
     sleep 5
     ((MAX_RUNS++))
 done
